619a46bdf3f0335d11b2e6955d55f9d770293575
TerSimaN <mcsiman998@gmail.com>
2024-04-22 21:41:03 +0300

    Refactored, added font

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 const Button = ({label = ""}) => {
   return (
-    <button className="px-3.5 py-3 border-2 border-black rounded-lg">
+    <button className="px-3.5 py-3 border-2 border-swamp-blue rounded-lg
+     text-left font-inter font-semibold text-base leading-[170%] text-swamp-blue">
         {label}
     </button>
   )
