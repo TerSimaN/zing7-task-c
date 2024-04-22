36b316324e98291f91a78b8c4aea88145c176c07
TerSimaN <mcsiman998@gmail.com>
2024-04-22 21:47:13 +0300

    Added hero implementation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,10 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="flex justify-center items-center w-full h-[234px]">
+        <h1 className="w-[931px] font-inter font-semibold text-center text-7xl leading-[108%] tracking-[-4px] text-swamp-blue">
+            Гъвкави решения за сухопътен транспорт за всеки клиент
+        </h1>
+    </div>
   )
 }
 
