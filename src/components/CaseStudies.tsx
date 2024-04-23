78f7bac0c29c48b5e1be1df5d4bceb3054a4e9ba
TerSimaN <mcsiman998@gmail.com>
2024-04-23 20:30:04 +0300

    Added case studies implementation

--- src/components/CaseStudies.tsx
+++ src/components/CaseStudies.tsx
@@ -1,7 +1,48 @@
+import { dangerousCargo, fridgeTransport, oversizeCargo } from "@/app/assets/images"
+import Image, { StaticImageData } from "next/image"
+
 const CaseStudies = () => {
-  return (
-    <div>CaseStudies</div>
-  )
+    const images: { iamgeSrc: StaticImageData, imageAlt: string, label: string }[] = [
+        {
+            iamgeSrc: dangerousCargo,
+            imageAlt: "dangerous cargo",
+            label: "Опасни товари (ADR)"
+        },
+        {
+            iamgeSrc: fridgeTransport,
+            imageAlt: "fridge transport",
+            label: "Хладилен транспорт"
+        },
+        {
+            iamgeSrc: oversizeCargo,
+            imageAlt: "oversize cargo",
+            label: "Извънгабаритни товари"
+        }
+    ]
+
+    return (
+        <div className="flex flex-col gap-y-16">
+            <h1 className="font-space-grotesk font-semibold text-5xl leading-[115%] tracking-[-2px] text-left text-swamp-blue">
+                Други услуги
+            </h1>
+            <div className="flex flex-col md:flex-row gap-x-6 gap-y-6">
+                {images.map((image) => (
+                    <div className="flex flex-col gap-y-2 md:gap-y-8">
+                        <Image
+                            src={image.iamgeSrc}
+                            alt={image.imageAlt}
+                            width={416}
+                            height={300}
+                            className="border rounded-2xl"
+                        />
+                        <h1 className="font-space-grotesk font-semibold text-2xl leading-[140%] tracking-[-0.5px] text-left text-swamp-blue">
+                            {image.label}
+                        </h1>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default CaseStudies
